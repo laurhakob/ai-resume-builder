67fa251a3a57d349cd9650f560920c01707f1be7
laurhakob <laurhakob@gmail.com>
2025-01-27 03:22:31 +0400

    stripe checkout(subscriptions)

--- src/components/premium/PremiumModal.tsx
+++ src/components/premium/PremiumModal.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { Check } from "lucide-react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
-import { Button } from "../ui/button";
+import { useToast } from "@/hooks/use-toast";
 import usePremiumModal from "@/hooks/usePremiumModal";
+import { Check } from "lucide-react";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { createCheckoutSession } from "./actions";
 
 const premiumFeatures = ["AI tools", "Up to 3 resumes"];
 const premiumPlusFeatures = ["Infinite resumes", "Design customizations"];
@@ -11,8 +14,35 @@ const premiumPlusFeatures = ["Infinite resumes", "Design customizations"];
 export default function PremiumModal() {
   const { open, setOpen } = usePremiumModal();
 
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState(false);
+
+  async function handlePremiumClick(priceId: string) {
+    try {
+      setLoading(true);
+      const redirectUrl = await createCheckoutSession(priceId);
+      window.location.href = redirectUrl;
+    } catch (error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again.",
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        if (!loading) {
+          setOpen(open);
+        }
+      }}
+    >
       <DialogContent className="max-w-2xl">
         <DialogHeader>
           <DialogTitle>Resume Builder AI Premium</DialogTitle>
@@ -25,27 +55,46 @@ export default function PremiumModal() {
               <ul className="list-inside space-y-2">
                 {premiumFeatures.map((feature) => (
                   <li key={feature} className="flex items-center gap-2">
-                    <Check className="size-4 text-green-700" />
+                    <Check className="size-4 text-green-500" />
                     {feature}
                   </li>
                 ))}
               </ul>
-              <Button>Get Premium</Button>
+              <Button
+                onClick={() =>
+                  handlePremiumClick(
+                    process.env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_MONTHLY!,
+                  )
+                }
+                disabled={loading}
+              >
+                Get Premium
+              </Button>
             </div>
             <div className="mx-6 border-l" />
             <div className="flex w-1/2 flex-col space-y-5">
-              <h3 className="bg-gradient-to-r from-purple-700 to-blue-400 bg-clip-text text-center text-lg font-bold text-transparent">
+              <h3 className="bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-center text-lg font-bold text-transparent">
                 Premium Plus
               </h3>
               <ul className="list-inside space-y-2">
                 {premiumPlusFeatures.map((feature) => (
                   <li key={feature} className="flex items-center gap-2">
-                    <Check className="size-4 text-purple-700" />
+                    <Check className="size-4 text-green-500" />
                     {feature}
                   </li>
                 ))}
               </ul>
-              <Button variant="premium">Get Premium Plus</Button>
+              <Button
+                variant="premium"
+                onClick={() =>
+                  handlePremiumClick(
+                    process.env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_PLUS_MONTHLY!,
+                  )
+                }
+                disabled={loading}
+              >
+                Get Premium Plus
+              </Button>
             </div>
           </div>
         </div>
