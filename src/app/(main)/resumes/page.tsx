cf6dec6096683e7e37252df226df4296a51620b2
laurhakob <laurhakob@gmail.com>
2025-01-28 00:32:32 +0400

    permission system

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -7,6 +7,8 @@ import { Metadata } from "next";
 import Link from "next/link";
 import ResumeItem from "./ResumeItem";
 import CreateResumeButton from "./CreateResumeButton";
+import { getUserSubscriptionLevel } from "@/lib/subscription";
+import { canCreateResume } from "@/lib/permissions";
 
 export const metadata: Metadata = {
   title: "Your resumes",
@@ -19,7 +21,7 @@ export default async function Page() {
     return null;
   }
 
-  const [resumes, totalCount] = await Promise.all([
+  const [resumes, totalCount, subscriptionLevel] = await Promise.all([
     prisma.resume.findMany({
       where: {
         userId,
@@ -34,13 +36,16 @@ export default async function Page() {
         userId,
       },
     }),
+    getUserSubscriptionLevel(userId),
   ]);
 
   // TODO: Check quota for non-premium users
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <CreateResumeButton canCreate={totalCount < 3} />
+      <CreateResumeButton
+        canCreate={canCreateResume(subscriptionLevel, totalCount)}
+      />
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your resumes</h1>
         <p>Total: {totalCount}</p>
